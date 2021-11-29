74e767c79206609088add8fad68ce3dbe874fa42
Fabien Verspan <78422154+fab971cool@users.noreply.github.com>
2021-11-29 16:16:34 +0100

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Launcher
@@ -7,11 +8,27 @@ public class Launcher
         System.out.println(" Bienvenue ! Votre commande :");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-        do
+        while (!line.equals("quit"))
         {
+
+
+            if (line.equals("fibo")) {
+                int n = scanner.nextInt();
+                String tmp = scanner.nextLine();
+                int f0 = 0, f1 = 1, fn = -1;
+
+                int i = 2;
+                for (; i <= n; i++) {
+                    fn = f0 + f1;
+                    f0 = f1;
+                    f1 = fn;
+                }
+                System.out.println(fn);
+                break;
+            }
             System.out.println(" Unknown Command ");
             line = scanner.nextLine();
 
-        }while (!line.equals("quit"));
+        }
     }
 }
